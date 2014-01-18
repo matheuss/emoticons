1568d3cc56fdecfd4306f0f5d95778d7d8d7ad6a
Matheus <matheus.frndes@gmail.com>
2014-01-18 02:02:32 -0200

    function initializeHTML

--- popup.js
+++ popup.js
@@ -3,9 +3,12 @@ _gaq.push(['_setAccount', 'UA-47156714-1']);
 _gaq.push(['_trackPageview']);
 
 (function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  var ga = document.createElement('script'); 
+  ga.type = 'text/javascript'; 
+  ga.async = true;
   ga.src = 'https://ssl.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  var s = document.getElementsByTagName('script')[0]; 
+  s.parentNode.insertBefore(ga, s);
 })();
 
 <script src="popup.js"></script>
--- script.js
+++ script.js
@@ -1,33 +1,66 @@
 window.onload = function () 
 {
+    initializeHTML();
+
     var divs = document.getElementsByClassName('text');
-    
-    var id;
 
     for(var i = 0; i < divs.length; i++)
     {      
         divs[i].onclick = function() 
         {
             selectText(this.id);
+            console.log("a");
             copy();
         }
     }
-
+}
 
 function selectText(id) 
 {
-        if (document.selection) 
-        {
-            var range = document.body.createTextRange();
-            range.moveToElementText(document.getElementById(id));
-            range.select();
-        } 
-        else if (window.getSelection) 
-        {
-            var range = document.createRange();
-            range.selectNode(document.getElementById(id));
-            window.getSelection().addRange(range);
-        }
+    if (document.selection) 
+    {
+        var range = document.body.createTextRange();
+        range.moveToElementText(document.getElementById(id));
+        range.select();
+    } 
+    else if (window.getSelection) 
+    {
+        var range = document.createRange();
+        range.selectNode(document.getElementById(id));
+        window.getSelection().addRange(range);
+    }
 }
+
+function initializeHTML(/*divs*/) 
+{
+    
+    //for(var i = 0; i < divs.length; i++)
+    var div = document.createElement('div');
+    div.setAttribute('id', 'shrug');
+    div.setAttribute('class', 'text');
+    div.innerHTML = '¯\\_(ツ)_/¯';
+
+    document.getElementsByTagName('body')[0].appendChild(div);
+    
+    div = document.createElement('div');
+    div.setAttribute('id', 'leface');
+    div.setAttribute('class', 'text');
+    div.innerHTML = '( ͡° ͜ʖ ͡°)';
+
+    document.getElementsByTagName('div')[0].parentNode.appendChild(div);
+
+    div = document.createElement('div');
+    div.setAttribute('id', 'registred');
+    div.setAttribute('class', 'text');
+    div.innerHTML = '®';
+
+    document.getElementsByTagName('div')[0].parentNode.appendChild(div);
+
+    div = document.createElement('div');
+    div.setAttribute('id', 'copyright');
+    div.setAttribute('class', 'text');
+    div.innerHTML = '©';
+
+    document.getElementsByTagName('div')[0].parentNode.appendChild(div);
 
 }
